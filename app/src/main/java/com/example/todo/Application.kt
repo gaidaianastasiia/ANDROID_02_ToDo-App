9fcbd08dcdedf195ab663dbc4add0fa77bed6435
Anastasia <haidaianastasiia@gmail.com>
2021-06-11 18:51:39 +0300

    added basic Dagger2 settings

---
+++ app/src/main/java/com/example/todo/Application.kt
@@ -0,0 +1,14 @@
+package com.example.todo
+
+import com.example.todo.di.component.DaggerApplicationComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+//Когда создаем свою реализацию Application, обязательно в manifest добавить
+//имя класса application: android:name=".Application"
+
+class Application: DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<Application?> {
+        return DaggerApplicationComponent.factory().create(this)
+    }
+}
